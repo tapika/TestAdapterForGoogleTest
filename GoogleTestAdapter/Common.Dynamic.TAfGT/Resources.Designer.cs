7cb0362bcca4d7ebc923a6549a4dab25be483996
Spencer Bloom <spebl@microsoft.com>
2017-10-03 19:51:58 -0700

    change default namespace to not conflict with common

--- GoogleTestAdapter/Common.Dynamic.TAfGT/Resources.Designer.cs
+++ GoogleTestAdapter/Common.Dynamic.TAfGT/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GoogleTestAdapter.Common {
+namespace GoogleTestAdapter.Common.Dynamic {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace GoogleTestAdapter.Common {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GoogleTestAdapter.Common.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GoogleTestAdapter.Common.Dynamic.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
