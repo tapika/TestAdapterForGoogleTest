08fd00839bd4798784249d3c22cfec1c58f9a8dd
Spencer Bloom <spebl@microsoft.com>
2017-10-04 15:55:39 -0700

    switch namespace back so it matches with translation files

--- GoogleTestAdapter/Common.Dynamic.GTA/Resources.Designer.cs
+++ GoogleTestAdapter/Common.Dynamic.GTA/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace GoogleTestAdapter.Common.Dynamic {
+namespace GoogleTestAdapter.Common {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace GoogleTestAdapter.Common.Dynamic {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GoogleTestAdapter.Common.Dynamic.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GoogleTestAdapter.Common.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- GoogleTestAdapter/Common/Resources.cs
+++ GoogleTestAdapter/Common/Resources.cs
@@ -31,7 +31,7 @@
                     // Get the path to GoogleTestAdapter.Common.Dynamic.dll where the resources are defined
                     var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "GoogleTestAdapter.Common.Dynamic.dll");
                     var asm = Assembly.LoadFrom(path);
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GoogleTestAdapter.Common.Dynamic.Resources", asm);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("GoogleTestAdapter.Common.Resources", asm);
                     resourceMan = temp;
                 }
                 return resourceMan;
