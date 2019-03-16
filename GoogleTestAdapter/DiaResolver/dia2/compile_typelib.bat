2b14541fa532226a5bc2eb5631143116cb413e0a
Tarmo Pikaro <tapika@yahoo.com>
2019-03-16 09:48:02 +0200

    Tune: Revert previous build fixes.

--- GoogleTestAdapter/DiaResolver/dia2/compile_typelib.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-if %VisualStudioVersion%. == . call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" >nul
-powershell -executionpolicy bypass -file compile_typelib.ps1
